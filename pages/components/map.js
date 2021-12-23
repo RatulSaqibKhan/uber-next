08389d2cb5de26ab112f0a24663226b124aff72f
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2021-12-24 02:34:53 +0600

    Uber Header and map integerated

---
+++ pages/components/map.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import tw from "tailwind-styled-components"
+import mapboxgl from "mapbox-gl"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicmF0dWxzYXFpYmtoYW4iLCJhIjoiY2t4amNrYTA0MDU1YzJ4b2J4NTVpcm1hZSJ9.mlsD20BdC9D85DZ6D3cy_g';
+
+const Map = () => {
+  
+  useEffect(() => {
+    const lng = 90.3816
+    const lat = 23.7618
+    const zoom = 12.11
+    new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  return (
+    <Wrapper id='map'>Map</Wrapper>
+  )
+}
+
+const Wrapper = tw.div`
+  flex-1
+`
+
+export default Map
